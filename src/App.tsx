3ad8b7888c2300ae9e182cf60dfb152d1221ed35
denys-zavhorodnii <dzavhorodnii@kse.org.ua>
2025-02-17 12:11:24 +0200

    prepare basic query

--- src/App.tsx
+++ src/App.tsx
@@ -7,3 +7,19 @@ export const App: React.FC = () => {
         </>
     );
 };
+
+const API_KEY = '';
+
+
+const params = new URLSearchParams();
+params.set('q', 'air');
+
+const link = `https://api.thecatapi.com/v1/breeds/search?${params.toString()}`;
+
+const options = {
+    headers: new Headers({
+        'x-api-key': API_KEY
+    })
+};
+
+fetch(link, options).then(console.log);
